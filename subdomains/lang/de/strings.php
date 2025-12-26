9f45509afac966fb38269039f25c93c6085c6836
JuggleFredJr <mail@philippstamp.de>
2025-12-26 16:45:49 +0100

    fixed small typos

--- subdomains/lang/de/strings.php
+++ subdomains/lang/de/strings.php
@@ -5,15 +5,15 @@ return [
     'domain' => 'Domain|Domains',
 
     'no_subdomains' => 'Keine Subdomains',
-    'subdomain' => 'Subdomain|Subdomawins',
+    'subdomain' => 'Subdomain|Subdomains',
     'limit' => 'Limit',
     'change_limit' => 'Limit anpassen',
     'limit_changed' => 'Limit angepasst',
-    'limit_reached' => 'Subdomainlmit erreicht',
+    'limit_reached' => 'Subdomainlimit erreicht',
     'create_subdomain' => 'Subdomain erstellen',
 
     'name' => 'Name',
 
     'api_token' => 'Cloudflare API Token',
-    'api_token_help' => 'Das Token benötigt Leseberechtigung für Zone.Zone und Schreibberechtigung für Zone.Dns. Für eine verstärkte Sicherheit können mit "Zone Resources" bestimmte Domains ausgeschlossen werden. Mit "Client IP Adress Filtering" kannst du die Panel-IP freigeben.',
+    'api_token_help' => 'Das Token benötigt Leseberechtigung für Zone.Zone und Schreibberechtigung für Zone.Dns. Für eine verstärkte Sicherheit, können mit "Zone Resources" bestimmte Domains ausgeschlossen werden. Mit "Client IP Adress Filtering" kannst du die Panel-IP freigeben.',
 ];

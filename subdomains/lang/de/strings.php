642933b55196057b177cfb5648b9d784cb308a75
Boy132 <mail@boy132.de>
2025-12-27 00:52:10 +0100

    small changes and fix pint

--- subdomains/lang/de/strings.php
+++ subdomains/lang/de/strings.php
@@ -9,11 +9,11 @@ return [
     'limit' => 'Limit',
     'change_limit' => 'Limit anpassen',
     'limit_changed' => 'Limit angepasst',
-    'limit_reached' => 'Subdomainlimit erreicht',
+    'limit_reached' => 'Subdomain Limit erreicht',
     'create_subdomain' => 'Subdomain erstellen',
 
     'name' => 'Name',
 
     'api_token' => 'Cloudflare API Token',
-    'api_token_help' => 'Das Token benötigt Leseberechtigung für Zone.Zone und Schreibberechtigung für Zone.Dns. Für eine verstärkte Sicherheit, können mit "Zone Resources" bestimmte Domains ausgeschlossen werden. Mit "Client IP Adress Filtering" kannst du die Panel-IP freigeben.',
+    'api_token_help' => 'Der Token benötigt Leseberechtigung für Zone.Zone und Schreibberechtigung für Zone.Dns. Für eine verbesserte Sicherheit können mit "Zone Resources" bestimmte Domains ausgeschlossen werden und die Panel-IP zum "Client IP Adress Filtering" hinzugefügt werden.',
 ];
